4e2b38c890aa0243d010e133467b98cc834a9bf1
Daniel Di Bella <daniel.lechner2@icloud.com>
2023-05-10 01:21:09 +0200

    made it possible to move in space

--- src/main/java/com/map_generation/View/SurfacePlot3D.java
+++ src/main/java/com/map_generation/View/SurfacePlot3D.java
@@ -2,58 +2,50 @@ package com.map_generation.View;
 
 import com.map_generation.Model.GenerateSimplexTiles;
 import com.map_generation.Model.Tile;
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.collections.ObservableFloatArray;
 import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.CullFace;
 import javafx.scene.shape.DrawMode;
 import javafx.scene.shape.MeshView;
 import javafx.scene.shape.TriangleMesh;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import org.fxyz3d.geometry.Point3D;
 
-import java.util.Random;
+import java.util.ArrayList;
 
 public class SurfacePlot3D extends Application {
 
     private static final int WIDTH = 800;
     private static final int HEIGHT = 600;
-    private static final int SIZE = 100;
-    private static final double SCALE = 100;
+    private static final int SIZE = 800;
+    private static final double SCALE = 1;
 
     @Override
     public void start(Stage primaryStage) {
-        Random rand = new Random();
+
         Group group = new Group();
-
-        GenerateSimplexTiles terrrain = new GenerateSimplexTiles(SIZE, SIZE, 8, 0.5);
-        Tile[][] tiles = terrrain.getTiles();
-
-        double[][] heightArray = new double[SIZE][SIZE];
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                heightArray[i][j] = rand.nextDouble(); // Generate a random height value between 0 and 1
-            }
-        }
+        Tile[][] tiles = new GenerateSimplexTiles(SIZE, SIZE, 8, .5).getTiles();
 
         TriangleMesh terrainMesh = new TriangleMesh();
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 float x = (float) (i * SCALE);
 
-                //System.out.println(tiles[i][j].value * SCALE);
-                //float y = (float) (tiles[i][j].value * SCALE);
+                //System.out.println(tiles[i][j].value * SCALE * 5);
+                float y = (float) (tiles[i][j].value * SCALE * 5);
 
-                System.out.println(heightArray[i][j] * SCALE);
-                float y = (float) (heightArray[i][j] * SCALE);
                 float z = (float) (j * SCALE);
                 terrainMesh.getPoints().addAll(x, y, z);
-                terrainMesh.getTexCoords().addAll((float) i / (SIZE - 1), (float) j / (SIZE - 1));
+                float color = (float) tiles[i][j].value / (float) SIZE; // calculate color based on height
+                terrainMesh.getTexCoords().addAll(color, color);
             }
         }
 
@@ -63,41 +55,81 @@ public class SurfacePlot3D extends Application {
                 int p1 = i * SIZE + j + 1;
                 int p2 = (i + 1) * SIZE + j + 1;
                 int p3 = (i + 1) * SIZE + j;
+
                 terrainMesh.getFaces().addAll(p0, 0, p3, 0, p1, 0);
                 terrainMesh.getFaces().addAll(p1, 0, p3, 0, p2, 0);
             }
         }
 
-
         MeshView terrain = new MeshView(terrainMesh);
         terrain.setCullFace(CullFace.BACK);
         terrain.setDrawMode(DrawMode.FILL);
 
-        group.getChildren().add(terrain);
 
+
+        group.getChildren().add(terrain);
         Scene scene = new Scene(group, WIDTH, HEIGHT, true);
 
-
-        // Animation
+        // Camera
 
         PerspectiveCamera camera = new PerspectiveCamera();
-        camera.setTranslateZ(-1000);
-        camera.setTranslateY(-700);
-        camera.setTranslateX(5000);
+        camera.setNearClip(0);
+        camera.setTranslateZ(0);
+        camera.setTranslateY(-500);
+        camera.setTranslateX(0);
         camera.setFieldOfView(60);
 
+        scene.setOnKeyPressed(event -> {
+            KeyCode keyCode = event.getCode();
+            double moveAmount = 15.0;
+            if (keyCode == KeyCode.A) {
+                camera.setTranslateX(camera.getTranslateX() - moveAmount);
+            } else if (keyCode == KeyCode.D) {
+                camera.setTranslateX(camera.getTranslateX() + moveAmount);
+            } else if (keyCode == KeyCode.W) {
+                camera.setTranslateZ(camera.getTranslateZ() + moveAmount);
+            } else if (keyCode == KeyCode.S) {
+                camera.setTranslateZ(camera.getTranslateZ() - moveAmount);
+            } else if (keyCode == KeyCode.Q) {
+                camera.setTranslateY(camera.getTranslateY() - moveAmount);
+            } else if (keyCode == KeyCode.Z) {
+                camera.setTranslateY(camera.getTranslateY() + moveAmount);
+            } else if (keyCode == KeyCode.LEFT) {
+                Rotate rotateY = new Rotate(-3, Rotate.Y_AXIS);
+                camera.getTransforms().addAll(rotateY);
+            } else if (keyCode == KeyCode.RIGHT) {
+                Rotate rotateY = new Rotate(3, Rotate.Y_AXIS);
+                camera.getTransforms().addAll(rotateY);
+            } else if (keyCode == KeyCode.UP) {
+                Rotate rotateY = new Rotate(-3, Rotate.X_AXIS);
+                camera.getTransforms().addAll(rotateY);
+            } else if (keyCode == KeyCode.DOWN) {
+                Rotate rotateY = new Rotate(3, Rotate.X_AXIS);
+                camera.getTransforms().addAll(rotateY);
+            } else if (keyCode == KeyCode.R) {
+                camera.setTranslateZ(0);
+                camera.setTranslateY(-500);
+                camera.setTranslateX(0);
+            }
+        });
+
         scene.setCamera(camera);
+
+
+        /*
+
+        Animation
 
         Timeline timeline = new Timeline();
 
         // Define the key frames for the animation
-        KeyValue xValue = new KeyValue(camera.translateXProperty(), 5000);
+        KeyValue xValue = new KeyValue(camera.translateXProperty(), 500);
         KeyValue yValue = new KeyValue(camera.translateYProperty(), -700);
         KeyValue zValue = new KeyValue(camera.translateZProperty(), -1000);
         KeyFrame startFrame = new KeyFrame(Duration.ZERO, xValue, yValue, zValue);
 
-        KeyValue xValue2 = new KeyValue(camera.translateXProperty(), 5000);
-        KeyValue yValue2 = new KeyValue(camera.translateYProperty(), -800);
+        KeyValue xValue2 = new KeyValue(camera.translateXProperty(), 500);
+        KeyValue yValue2 = new KeyValue(camera.translateYProperty(), -700);
         KeyValue zValue2 = new KeyValue(camera.translateZProperty(), 10000);
         KeyFrame endFrame = new KeyFrame(Duration.seconds(60), xValue2, yValue2, zValue2);
 
@@ -108,11 +140,14 @@ public class SurfacePlot3D extends Application {
 
         // Start the animation
         timeline.play();
+        */
+
 
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Height Array Terrain");
+        primaryStage.setTitle("3D Visualize");
         primaryStage.show();
     }
+
 
     public static void main(String[] args) {
         launch(args);
